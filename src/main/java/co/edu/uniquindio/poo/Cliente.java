83bca3b164ba40627f1ab71364e96ae2e32e2508
Byron-Prieto <160172778+Byron-Prieto@users.noreply.github.com>
2024-10-07 20:20:56 -0500

    Cliente

---
+++ src/main/java/co/edu/uniquindio/poo/Cliente.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.poo;
+
+public class Cliente{
+
+    String Name;
+    String LastName;
+
+    public Cliente(String Name, String LastName){
+
+        this.Name = Name;
+        this.LastNAme = LastNAme;
+    }
+}
+
