62e3f6e3788207201a4567bca8b1da8d72000752
juandanielvera <juand.veram1@uqvirtual.edu.co>
2024-10-07 20:25:56 -0500

    commit

--- src/main/java/co/edu/uniquindio/poo/Persona.java
+++ src/main/java/co/edu/uniquindio/poo/Persona.java
@@ -1,7 +1,29 @@
 package co.edu.uniquindio.poo;
 
 public class Persona {
-    public  String CorreoElectronico;
-    public  int IDCliente;
+    public  String Nombre;
+    public  int Teléfono;
+    public Persona(String nombre, int teléfono) {
+        Nombre = nombre;
+        Teléfono = teléfono;
+    }
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public int getTeléfono() {
+        return Teléfono;
+    }
+    public void setTeléfono(int teléfono) {
+        Teléfono = teléfono;
+    }
+    
+    public void GetNombre(){
+        System.out.println();
+       
+    }
+   
     
 }
